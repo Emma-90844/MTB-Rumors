92b5226e30696952954058c5226d1f45045a8f7d
Emma-90844 <enabojohn@gmail.com>
2022-03-07 09:22:51 +0300

    Version 1.0.0

--- api/index.js
+++ api/index.js
@@ -1,34 +1,76 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/users');
-const postRoute = require('./routes/posts')
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path")
+const  bodyParser = require('body-parser')
+
+// ROUTES.
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
 
 const app = express();
 
-
-// Configurations
+// CONFIGURATIONS.
 dotenv.config();
 app.use(express.json());
 
 
-// DB Connection
-mongoose.connect(process.env.MONGO_URL,
-   { useNewUrlParser: true,
-    useUnifiedTopology:true,
-}).then(console.log('connected to MONGO DB'))
-.catch((err) => console.log(err))
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 
-// Middle wares
-app.use('/api/auth', authRoute);
-app.use('/api/users', userRoute);
-app.use('/api/posts', postRoute);
 
-app.listen('5000', (req, res) => {
-    console.log('Backend listening to PORT: 5000')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+
+// DB Connection.
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify:true
+  })
+  .then(console.log("connected to MONGO DB"))
+  .catch((err) => console.log(err));
+
+//MULTER image store.
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
 
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been downloaded");
+});
+
+// MIDDLE wares
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 
+// SERVE STATIC ASSETS FOLDER
+if(process.env.NODE_ENV === 'production'){
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+app.listen("5000", (req, res) => {
+  console.log("Backend listening to PORT: 5000");
+});
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 //  REGISTER
-router.post("/register", async (req, res) => {
+router.post("/signup", async (req, res) => {
+
   try {
     const salt = await bcrypt.genSalt(10);
     const hashPass = await bcrypt.hash(req.body.password, salt);
+    
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -13,8 +15,8 @@ router.post("/register", async (req, res) => {
     });
 
     const user = await newUser.save();
-    console.log(user);
-    res.status(200).json(user);
+    
+    return res.status(200).json(user);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -22,14 +24,21 @@ router.post("/register", async (req, res) => {
 
 // LOGIN
 router.post("/login", async (req, res) => {
+  console.log(req.body);
   try {
+   
     const user = await User.findOne({ username: req.body.username });
+ 
     !user && res.status(400).json("Incorrect Credentials");
+    
     const validated = await bcrypt.compare(req.body.password, user.password);
+
     !validated && res.status(400).json("Incorrect Credentials");
 
     const {password, ...others} = user._doc; //filters out the password prop not to be sent to user
+    
     res.status(200).json(others);
+
   } catch (err) {
     res.status(500).json(err);
   }
